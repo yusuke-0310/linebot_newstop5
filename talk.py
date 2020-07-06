911fa1029a99d5dbeffa0b0a476ea6b1f069de45
Yusuke <yuui0ig@gmail.com>
2020-07-06 22:30:06 +0900

    second commit

--- talk.py
+++ talk.py
@@ -4,5 +4,5 @@ def create_reply(user_text):
     apikey = "DZZPKSBU5XUE5wY9tfXf4QCEuOdg36C1"
     client = pya3rt.TalkClient(apikey)
 
-    res = client.talk("user_text")
+    res = client.talk(user_text)
     return res['results'][0]['reply']
