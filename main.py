e6ef4ca9b34e1d814f7445a10646709b5ecf102c
Yusuke <yuui0ig@gmail.com>
2020-08-29 12:06:03 +0900

    newsadd63

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ def handle_message(event):
     reply = create_reply(event.message.text)
     news = newstop5(event.message.text)
     #event.message.textにLINEで送ったメッセージが入る
-    while True:
+    for i in news:
         return line_bot_api.reply_message(event.reply_token, TextSendMessage(text=i))
     #replyにcreate_replyの内容が反映される。replyに仮にevent.message.textを入れると送った内容がそのまま返信される
 
